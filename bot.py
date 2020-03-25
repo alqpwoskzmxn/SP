b88212b07f31bcdb4eb5dd25ea113f364087d869
alqpwoskzmxn <audrb30@naver.com>
2020-03-25 17:29:26 +0000

    add topic modeling skeleton code

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import discord
 from konlpy.tag import Okt
-import nltk
+from gensim.models import word2vec
 
 okt = Okt()
+model = ''
+
 
 class MyClient(discord.Client):
     messagelist = []
+    model = ''
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -14,17 +17,22 @@ class MyClient(discord.Client):
             return
 
         if message.content == '>topic':
-            await message.channel.send('The Topic is ' + self.messagelist[0])
+            #await message.channel.send('The topic is ' + str([ word[0].split('/')[0] for word in self.model.most_similar('업무', topn=3)]))
+            a = ''
         elif message.content == '>train':
             await message.channel.send('Training')
-            text = nltk.Text(self.messagelist, name='NMSC')
-            print(text.vocab().most_common(10))
+            #self.model = word2vec.Word2Vec(self.messagelist, size=100, window=3, iter=100)
+            await message.channel.send('Training end')
+        elif message.content == '>reset':
+            self.model = ''
+            self.messagelist = []
         else:
-            self.messagelist = self.messagelist + okt.pos(message.content, norm=True, stem=True, join=True)
-            print(self.messagelist)
+            self.messagelist = self.messagelist + \
+                okt.pos(message.content, norm=True, stem=True, join=True)
 
     async def on_typing(self, channel, user, when):
         print(user.name + '(' + str(user.id) + ') is typing')
+
 
 client = MyClient()
 client.run(open('token', 'r').read())
