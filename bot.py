61dc0a110f2feda89700a1e3086808b8f5e56234
alqpwoskzmxn <audrb30@naver.com>
2020-03-25 13:41:56 +0000

    text preprocessing

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
+from konlpy.tag import Okt
+import nltk
+
+okt = Okt()
 
 class MyClient(discord.Client):
-    s = ''
+    messagelist = []
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -9,16 +13,18 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content.startswith('>'):
-            await message.channel.send('This is Command')
-            print('Command Input')
+        if message.content == '>topic':
+            await message.channel.send('The Topic is ' + self.messagelist[0])
+        elif message.content == '>train':
+            await message.channel.send('Training')
+            text = nltk.Text(self.messagelist, name='NMSC')
+            print(text.vocab().most_common(10))
         else:
-            self.s += message.content
+            self.messagelist = self.messagelist + okt.pos(message.content, norm=True, stem=True, join=True)
+            print(self.messagelist)
 
     async def on_typing(self, channel, user, when):
         print(user.name + '(' + str(user.id) + ') is typing')
-        await channel.send('typing')
 
 client = MyClient()
 client.run(open('token', 'r').read())
-
